d1ecd34f451b79b1180e328fca9d0a2386f2d776
yinshunyao <yinshunyao@qq.com>
2017-03-09 18:49:38 +0800

    init
    安装程序制作

---
+++ conf/fdfs/stop.sh
@@ -0,0 +1,10 @@
+
+
+#fdfsdfs auto start sh stop #####################################################
+sudo killall fdfs_trackerd
+sudo killall fdfs_storaged
+sudo killall nginx
+#fdfsdfs auto start sh end #####################################################
+
+
+
